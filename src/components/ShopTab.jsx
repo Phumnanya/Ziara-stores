7c8926b87c98a52fde2de94b206b74ab567f5f83
Eustace Mbanefo <111151661+Phumnanya@users.noreply.github.com>
2025-08-19 16:21:02 +0100

    Update ShopTab.jsx

--- src/components/ShopTab.jsx
+++ src/components/ShopTab.jsx
@@ -3,54 +3,54 @@ import Itembox from "../assets/body-comps/Itembox";
 
 function Shoptab() {
     const Women = [
-        { id: 0, pic: "reza-delkhosh-1h4SHm3SZ0c-unsplash.jpg", fashion: "FASHION",
+        { id: 0, pic: "https://ik.imagekit.io/fvrl067t2/ziara/reza-delkhosh-1h4SHm3SZ0c-unsplash.jpg?updatedAt=1755611628792", fashion: "FASHION",
             name: "Fendi All-in-one", rating: 4, price:75
          },
-        { id: 1, pic: "reza-delkhosh-iRAOJYtPHZE-unsplash.jpg", fashion: "FASHION",
+        { id: 1, pic: "https://ik.imagekit.io/fvrl067t2/ziara/reza-delkhosh-iRAOJYtPHZE-unsplash.jpg?updatedAt=1755611612493", fashion: "FASHION",
             name: "Fendi All-in-one", rating: 4, price:83
          },
-        { id: 2, pic: "raamin-ka-uR51HXLO7G0-unsplash.jpg", fashion: "FASHION",
+        { id: 2, pic: "https://ik.imagekit.io/fvrl067t2/ziara/raamin-ka-uR51HXLO7G0-unsplash.jpg?updatedAt=1755611616340", fashion: "FASHION",
             name: "Fendi All-in-one", rating: 4, price:70
          },
-        { id: 3, pic: "ospan-ali-nyrSsBzhZ4Y-unsplash.jpg", fashion: "FASHION",
+        { id: 3, pic: "/img/ospan-ali-nyrSsBzhZ4Y-unsplash.jpg", fashion: "FASHION",
             name: "Fendi All-in-one", rating: 4, price:45
          },
-        { id: 4, pic: "aiony-haust-IXYxqP4zejo-unsplash.jpg", fashion: "FASHION",
+        { id: 4, pic: "https://ik.imagekit.io/fvrl067t2/ziara/aiony-haust-IXYxqP4zejo-unsplash.jpg?updatedAt=1755611314671", fashion: "FASHION",
             name: "Fendi All-in-one", rating: 4, price:75
          },
-        { id: 5, pic: "alexanderafan-68EiF7-sU7w-unsplash.jpg", fashion: "FASHION",
+        { id: 5, pic: "https://ik.imagekit.io/fvrl067t2/ziara/alexanderafan-68EiF7-sU7w-unsplash.jpg?updatedAt=1755611335742", fashion: "FASHION",
             name: "Fendi All-in-one", rating: 4, price:75
          },
-        { id: 6, pic: "dom-hill-nimElTcTNyY-unsplash.jpg", fashion: "FASHION",
+        { id: 6, pic: "https://ik.imagekit.io/fvrl067t2/ziara/dom-hill-nimElTcTNyY-unsplash.jpg?updatedAt=1755611334546", fashion: "FASHION",
             name: "Fendi All-in-one", rating: 4, price:75
          },
-        { id: 7, pic: "premium_photo-1708276242787-387acf1bbd4b.jpeg", fashion: "FASHION",
+        { id: 7, pic: "https://ik.imagekit.io/fvrl067t2/ziara/premium_photo-1708276242787-387acf1bbd4b.jpeg?updatedAt=1755611577571", fashion: "FASHION",
             name: "Fendi All-in-one", rating: 4, price:75
          },
     ]
     const Men = [
-        { id: 0, pic: "premium_photo-1707932485795-1d0aed727376.jpeg", fashion: "FASHION",
+        { id: 0, pic: "https://ik.imagekit.io/fvrl067t2/ziara/premium_photo-1707932485795-1d0aed727376.jpeg?updatedAt=1755611578042", fashion: "FASHION",
             name: "Fendi All-in-one", rating: 4, price:75
          },
-        { id: 1, pic: "premium_photo-1675130119382-6f891206f406.jpeg", fashion: "FASHION",
+        { id: 1, pic: "https://ik.imagekit.io/fvrl067t2/ziara/premium_photo-1675130119382-6f891206f406.jpeg?updatedAt=1755611572606", fashion: "FASHION",
             name: "Fendi All-in-one", rating: 4, price:83
          },
-        { id: 2, pic: "mohamad-khosravi-5KyzZbonwqQ-unsplash.jpg", fashion: "FASHION",
+        { id: 2, pic: "https://ik.imagekit.io/fvrl067t2/ziara/mohamad-khosravi-5KyzZbonwqQ-unsplash.jpg?updatedAt=1755611514132", fashion: "FASHION",
             name: "Fendi All-in-one", rating: 4, price:70
          },
-        { id: 3, pic: "mohamad-khosravi-zCSlmX3-98I-unsplash.jpg", fashion: "FASHION",
+        { id: 3, pic: "https://ik.imagekit.io/fvrl067t2/ziara/mohamad-khosravi-zCSlmX3-98I-unsplash.jpg?updatedAt=1755611523671", fashion: "FASHION",
             name: "Fendi All-in-one", rating: 4, price:45
          },
-        { id: 4, pic: "patrick-amofah-cytUXI-fEOU-unsplash.jpg", fashion: "FASHION",
+        { id: 4, pic: "https://ik.imagekit.io/fvrl067t2/ziara/patrick-amofah-cytUXI-fEOU-unsplash.jpg?updatedAt=1755611599531", fashion: "FASHION",
             name: "Fendi All-in-one", rating: 4, price:75
          },
-        { id: 5, pic: "omid-bonyadian-lQ0bz51_cXM-unsplash.jpg", fashion: "FASHION",
+        { id: 5, pic: "https://ik.imagekit.io/fvrl067t2/ziara/omid-bonyadian-lQ0bz51_cXM-unsplash.jpg?updatedAt=1755611602370", fashion: "FASHION",
             name: "Fendi All-in-one", rating: 4, price:75
          },
-        { id: 6, pic: "yasamine-june-tOubjwKS6f0-unsplash.jpg", fashion: "FASHION",
+        { id: 6, pic: "https://ik.imagekit.io/fvrl067t2/ziara/yasamine-june-tOubjwKS6f0-unsplash.jpg?updatedAt=1755611632728", fashion: "FASHION",
             name: "Fendi All-in-one", rating: 4, price:75
          },
-        { id: 7, pic: "robiul-islam-pailot-vqyEjQlmps8-unsplash.jpg", fashion: "FASHION",
+        { id: 7, pic: "/img/robiul-islam-pailot-vqyEjQlmps8-unsplash.jpg", fashion: "FASHION",
             name: "Fendi All-in-one", rating: 4, price:75
          },
     ]
@@ -106,5 +106,6 @@ function Shoptab() {
         </>
     )
 }
+
 
 export default Shoptab;
