e325326ea429f2b65292f86708388ef8edf0e261
Eustace Mbanefo <111151661+Phumnanya@users.noreply.github.com>
2025-08-19 15:00:51 +0100

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { ChakraProvider } from '@chakra-ui/react';
+import { BrowserRouter } from 'react-router-dom';
+import './base.css'
 import App from './App';
+import './index.css'
+import MyTheme from './theme/MyTheme';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
-  <ChakraProvider>
-    <App />
+  <ChakraProvider theme={MyTheme}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </ChakraProvider>
 );
